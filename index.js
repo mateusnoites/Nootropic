7051e2e97b59239e948f27cd9f632a5c3485af27
Mateus Dias <dias.mateus@escolar.ifrn.edu.br>
2023-10-16 22:37:51 -0300

    Páginas Home, Cadastro e Login finalizadas.

--- index.js
+++
@@ -1,8 +0,0 @@
-const connectToDatabase = require("./src/database/connect");
-
-const dotenv = require("dotenv")
-dotenv.config()
-
-connectToDatabase();
-
-require("./modules/express");
--- src/database/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectToDatabase = async() => {
-  await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.btpo07m.mongodb.net/?retryWrites=true&w=majority`).then(()=> {
-    console.log("Conexão ao banco de dados realizada com sucesso!");
-  })
-}
-
-module.exports = connectToDatabase
